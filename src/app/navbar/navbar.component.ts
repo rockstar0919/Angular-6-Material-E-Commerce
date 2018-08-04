9b4863d99ae58876830e7e3800fe25cae77408e6
rockstar0919 <rock.developer0919@gmail.com>
2018-08-05 00:24:25 +0530

    Displaying current User Name from Google

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  user: firebase.User;
   constructor(private afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(info => {
-      console.log(info);
-    });
+    this.afAuth.authState.subscribe(user => this.user = user);
   }
 
 
