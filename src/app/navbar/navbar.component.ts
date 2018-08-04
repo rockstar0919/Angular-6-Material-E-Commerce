1906a24a9e2a020ae80e6e9239386ba785a603c3
rockstar0919 <rock.developer0919@gmail.com>
2018-08-05 01:18:00 +0530

    using async pipe - Logout 0- vid 290

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  user: firebase.User;
+  user$: Observable<firebase.User>;
   constructor(private afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(user => this.user = user);
+    // before async pipe
+    // this.afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = this.afAuth.authState;
   }
 
 
