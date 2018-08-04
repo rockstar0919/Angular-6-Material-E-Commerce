ee63469ee56284546f7a14923e5b5d377100b848
rockstar0919 <rock.developer0919@gmail.com>
2018-08-04 23:54:57 +0530

    logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(info => {
+      console.log(info);
 
-  ngOnInit() {
+    });
   }
 
+
+
+  logout() {
+    this.afAuth.auth.signOut();
+
+  }
 }
