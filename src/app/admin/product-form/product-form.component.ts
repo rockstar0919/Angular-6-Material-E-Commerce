c768aeec59b7f19d3238709b76ccfef83235a2cf
rockstar0919 <rock.developer0919@gmail.com>
2018-08-06 01:21:33 +0530

    306 - WIP - updating products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ProductService } from './../../product.service';
 import { CategoryService } from './../../category.service';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
@@ -11,12 +12,25 @@ import { Component, OnInit } from '@angular/core';
 export class ProductFormComponent implements OnInit {
 
   categories = [];
-  constructor(private categoryServ: CategoryService, private prodServ: ProductService, private router: Router) {
+  title;
+  price;
+  imageUrl;
+  category;
+  productForm: FormControl;
 
+  noImageUrl = '../../../assets/img/noImg.jpg';
+  // tslint:disable-next-line:max-line-length
+  constructor(private route: ActivatedRoute, private categoryServ: CategoryService, private prodServ: ProductService, private router: Router) {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.getProductById(id);
+    }
 
   }
 
   ngOnInit() {
+
+
     this.categoryServ.getCategories().subscribe(categories => {
       categories.forEach(x => {
         const obj = x.payload.toJSON();
@@ -29,6 +43,18 @@ export class ProductFormComponent implements OnInit {
   save(product) {
     this.prodServ.create(product);
     this.router.navigate(['/admin/products']);
+  }
+
+  getProductById(id) {
+    this.prodServ.getProductById(id).subscribe(data => {
+
+      const product = data.payload.toJSON();
+      this.title = product['title'];
+      this.price = product['price'];
+      this.imageUrl = product['imageUrl'];
+      this.category = product['category'];
+
+    });
   }
 
 }
